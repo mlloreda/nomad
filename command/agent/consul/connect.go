e6102414f78236d5e72b9eaa3898aaaadfa8e70d
Seth Hoenig <shoenig@hashicorp.com>
2021-05-06 15:42:03 -0600

    wip lsadjfsdlkfj

--- command/agent/consul/connect.go
+++ command/agent/consul/connect.go
@@ -198,7 +198,7 @@ func connectUpstreams(in []structs.ConsulUpstream) []api.Upstream {
 			LocalBindPort:    upstream.LocalBindPort,
 			Datacenter:       upstream.Datacenter,
 			LocalBindAddress: upstream.LocalBindAddress,
-			// MeshGateway:      connectMeshGateway(upstream.MeshGateway),
+			MeshGateway:      connectMeshGateway(upstream.MeshGateway),
 		}
 	}
 	return upstreams
